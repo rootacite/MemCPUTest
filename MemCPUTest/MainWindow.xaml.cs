de4c7f9b2a0be1f7edcd7d5c226acc917dce3c92
14980 <14980@LISHIYUAN>
2021-08-17 15:59:53 +0800

    添加项目文件。

---
+++ MemCPUTest/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace MemCPUTest
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("Patch.dll")]
+        extern static ulong getAvailablePhysMemBytes();
+        [DllImport("Patch.dll")]
+        extern static ulong Test();
+        [DllImport("Patch.dll")]
+        extern static void FreeMems();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DispatcherTimer t = new DispatcherTimer();
+            t.Interval = TimeSpan.FromSeconds(1);
+            t.Tick += (f, v) =>
+            {
+                ulong left = getAvailablePhysMemBytes();
+
+                LeftMem.Text = left.ToString() + "(" + Math.Round(left / (1024d * 1024d * 1024d), 2).ToString() + "GB" + ")";
+            };
+
+            t.Start();
+        }
+
+        private void Start_Click(object sender, RoutedEventArgs e)
+        {
+            Start.IsEnabled = false;
+            Start.Content = "进行中......";
+
+             new Thread(() =>
+            {
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+
+                ulong rest = Test();
+
+                sw.Stop();
+                long time = sw.ElapsedMilliseconds;
+
+                this.Dispatcher.Invoke(() =>
+                {
+                    Start.Content = "完成";
+                    Refmem.Text = rest.ToString() + "(" + Math.Round(rest / (1024d), 2).ToString() + "GB" + ")";
+                    TS.Text = (time / (double)rest).ToString() + "ms";
+                    MenRet.Text = ((((double)rest / (double)time))*1000d).ToString() + "MB/s";
+                });
+
+                FreeMems();
+            })
+            { IsBackground=true}.Start();
+        }
+    }
+}
---
+++ Patch/dllmain.cpp
@@ -0,0 +1,62 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+extern "C" __declspec(dllexport) unsigned long long getAvailablePhysMemBytes()
+{
+    static unsigned long long size = 0;
+
+    MEMORYSTATUSEX memoryInfo;
+    memoryInfo.dwLength = sizeof(memoryInfo);
+    GlobalMemoryStatusEx(&memoryInfo);
+    size = memoryInfo.ullAvailPhys;//)/1024/1024
+    return size;
+}
+
+
+vector<LPVOID> mems(0);
+
+extern "C" __declspec(dllexport) 
+unsigned long long Test(){
+ 
+  
+
+    void* n_point = NULL;
+    do {
+        n_point = malloc(1024*1024);
+        mems.push_back(n_point);
+    } while (n_point != NULL && getAvailablePhysMemBytes ()>1024*1024*10);
+
+    unsigned long long r;
+    r = mems.size();
+
+    return r;
+}
+
+
+extern "C" __declspec(dllexport) void FreeMems() {
+
+    unsigned long long r;
+    r = mems.size();
+    for (int i = 0; i < r; i++) {
+        free(mems[i]);
+    }
+}
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+                     )
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
